7b89e7648917b6eabf678a0660e99f3752867578
NicolasMontone <montonenicolas01@gmail.com>
2024-11-25 10:40:23 -0300

    add metadata for page title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { FourPhotosOneStartup } from '@/components/four-photos-one-startup'
 
+export const metadata = {
+  title: '4 photos 1 startup',
+  description: 'a game to guess the startup from 4 photos',
+}
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 py-12">
